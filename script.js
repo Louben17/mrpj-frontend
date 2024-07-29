f49c4af090e7483c0100f2f05959114476de626e
Louben17 <141031342+Louben17@users.noreply.github.com>
2024-07-29 11:02:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ async function fetchInstagramFeed() {
     try {
         const response = await fetch('https://mrpj-backend.vercel.app/instagram-feed');
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error(`Network response was not ok: ${response.statusText}`);
         }
         const data = await response.json();
+        if (!Array.isArray(data)) {
+            throw new Error('Returned data is not an array');
+        }
+        
         const feedContainer = document.getElementById('instagram-feed');
+        if (!feedContainer) {
+            throw new Error('Instagram feed container not found');
+        }
+        
+        feedContainer.innerHTML = ''; // Clear any previous content
 
         data.forEach(post => {
             const img = document.createElement('img');
