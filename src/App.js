333b0e0563d067f4ad091e0d2d4cee47f76d581f
Louben17 <141031342+Louben17@users.noreply.github.com>
2024-10-03 14:03:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,50 +3,47 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-    const [instagramPosts, setInstagramPosts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [instagramPosts, setInstagramPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-useEffect(() => {
+  useEffect(() => {
     const fetchInstagramPosts = async () => {
-        try {
-            const response = await axios.get('https://mrpj-backend.vercel.app/api/instagram');
-            const allPosts = response.data; // Vezmi všechny příspěvky
-
-            // Vyber pouze první 4 příspěvky
-            const limitedPosts = allPosts.slice(0, 4);
-            setInstagramPosts(limitedPosts);
-            setLoading(false);
-        } catch (error) {
-            console.error('Error fetching Instagram posts:', error);
-            setError('Nepodařilo se načíst Instagram příspěvky. Zkuste to prosím později.');
-            setLoading(false);
-        }
+      try {
+        const response = await axios.get('https://mrpj-backend.vercel.app/api/instagram');
+        const imagePosts = response.data.slice(0, 4);
+        setInstagramPosts(imagePosts);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching Instagram posts:', error);
+        setError('Nepodařilo se načíst Instagram příspěvky. Zkuste to prosím později.');
+        setLoading(false);
+      }
     };
 
     fetchInstagramPosts();
-}, []);
+  }, []);
 
-return (
-  <div className="instagram-feed">
-    {loading && <p>Načítání Instagram příspěvků...</p>}
-    {error && <p className="error">{error}</p>}
-    {instagramPosts.slice(0, 4).map((post) => (
-      <div key={post.id} className="instagram-post">
-        <a href={post.permalink} target="_blank" rel="noopener noreferrer">
-          {post.media_type === 'IMAGE' ? (
-            <img src={post.media_url} alt={post.caption} />
-          ) : post.media_type === 'REEL' ? (
-            <video controls style={{ display: 'block' }} muted>
-              <source src={post.media_url} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          ) : null}
-        </a>
-      </div>
-    ))}
-  </div>
-);
-
+  return (
+    <div className="instagram-feed">
+      {loading && <p>Načítání Instagram příspěvků...</p>}
+      {error && <p className="error">{error}</p>}
+      {instagramPosts.map((post) => (
+        <div key={post.id} className="instagram-post">
+          <a href={post.permalink} target="_blank" rel="noopener noreferrer">
+            {post.media_type === 'IMAGE' ? (
+              <img src={post.media_url} alt={post.caption} />
+            ) : post.media_type === 'REEL' ? (
+              <video controls muted style={{ display: 'block' }}>
+                <source src={post.media_url} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            ) : null}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default App;
