62098814b3e482e834db5ed61604c71ab6b0685d
Louben17 <141031342+Louben17@users.noreply.github.com>
2024-10-03 13:24:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,26 +23,26 @@ function App() {
     fetchInstagramPosts();
   }, []);
 
-  return (
+return (
     <div className="instagram-feed">
-      {loading && <p>Načítání Instagram příspěvků...</p>}
-      {error && <p className="error">{error}</p>}
-      {instagramPosts.map((post) => (
-        <div key={post.id} className="instagram-post">
-          <a href={post.permalink} target="_blank" rel="noopener noreferrer">
-            {post.media_type === 'IMAGE' ? (
-              <img src={post.media_url} alt={post.caption} />
-            ) : post.media_type === 'REEL' ? (
-              <video controls>
-                <source src={post.media_url} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-            ) : null}
-          </a>
-        </div>
-      ))}
+        {loading && <p>Načítání Instagram příspěvků...</p>}
+        {error && <p className="error">{error}</p>}
+        {instagramPosts.map((post) => (
+            <div key={post.id} className="instagram-post">
+                <a href={post.permalink} target="_blank" rel="noopener noreferrer">
+                    {post.media_type === 'VIDEO' ? (
+                        <video controls>
+                            <source src={post.media_url} type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    ) : (
+                        <img src={post.media_url} alt={post.caption} />
+                    )}
+                </a>
+            </div>
+        ))}
     </div>
-  );
-}
+);
+
 
 export default App;
